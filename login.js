693b66114c70545c26ed452db99e08812e3ae41e
TechstaZack <114269271+TechstaZack@users.noreply.github.com>
2023-02-04 19:16:25 +1100

    Add files via upload

---
+++ login.js
@@ -0,0 +1,8 @@
+function addUser() {
+    name1 = document.getElementById('player1_name_input').value;
+    name2 = document.getElementById('player2_name_input').value;
+    localStorage.setItem("name1", name1);
+    localStorage.setItem("name2", name2);
+
+    window.location = "game_page.html"
+}
